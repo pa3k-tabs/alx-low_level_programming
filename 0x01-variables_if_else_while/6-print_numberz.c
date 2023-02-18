675d0613ba97335b835db3a0188557ba19bdcdef
Epor <epoxide7@gmail.com>
2023-02-17 21:55:55 -0800

    corrected

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,9 +1,4 @@
-/*
- * File: 6-print_numberz.c
- * Auth: Brennan D Baraban
- */
-
-#include <stdio.h>
+6-print_numberz.c#include <stdio.h>
 
 /**
  * main - Prints all single digit numbers of base 10 starting from 0,
