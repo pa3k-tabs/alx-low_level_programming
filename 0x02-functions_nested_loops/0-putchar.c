89670c8ced05e86119a0bcd84dcada4d6b9006de
Epor <epoxide7@gmail.com>
2023-02-21 10:34:24 -0800

    ab

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,13 @@
-#include "holberton.h"
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	char *sh = "Holberton";
-
-	while (*sh)
+#include "main.h"
+	#include <unistd.h>
+	/**
+	 * _putchar - writes the character c to stdout
+	 * @c: The character to print
+	 *
+	 * Return: On success 1.
+	 * On error, -1 is returned, and errno is set appropriately.
+	 */
+	int _putchar(char c)
 	{
-		_putchar(*sh);
-		sh++;
+		return (write(1, &c, 1));
 	}
-	_putchar('\n');
-
-	return (0);
-}
