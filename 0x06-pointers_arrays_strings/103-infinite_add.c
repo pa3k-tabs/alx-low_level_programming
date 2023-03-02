2c0729d1027532dc25a6ee00c5ea575240f3f746
Epor <epoxide7@gmail.com>
2023-03-02 10:52:34 -0800

    abcd

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -25,7 +25,6 @@ void rev_string(char *n)
 		*(n + i) = temp;
 	}
 }
-
 /**
  * infinite_add - add 2 numbers together
  * @n1: text representation of 1st number to add
@@ -34,7 +33,6 @@ void rev_string(char *n)
  * @size_r: buffer size
  * Return: pointer to calling function
  */
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int overflow = 0, i = 0, j = 0, digits = 0;
